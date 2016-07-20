b7a97a17d8cb8e9fde778bc197c806927bfdf1c5
cristhianzb <cristhianzentenob@gmail.com>
2016-07-19 23:50:57 -0400

    AcercaDe
    Avance de Acerca de

--- Tetris/src/crj/zhz/android/tetris/MainActivity.java
+++ Tetris/src/crj/zhz/android/tetris/MainActivity.java
@@ -1,9 +1,11 @@
 package crj.zhz.android.tetris;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -30,5 +32,10 @@ public class MainActivity extends Activity {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
+	}
+	
+	public void lanzarAcercaDe(View view){
+		Intent i = new Intent(this,AcercaDe.class);
+		startActivity(i);
 	}
 }
